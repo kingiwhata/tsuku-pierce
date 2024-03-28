4052a6e2ed3f1a129d38696deee211ec25abe2c8
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-03-29 01:49:16 +1300

    about

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import cart from "./assets/pierce-cart.svg";
 import search from "./assets/pierce-search.svg";
@@ -22,7 +22,6 @@ function App() {
         "JOYFUL",
     ];
     const [moveText, setText] = useState("PROUD");
-    const nextText = "JOYFUL";
     function triggerMove() {
         const randomIndex = Math.floor(Math.random() * whyArr.length);
         const iCheck = whyArr.indexOf(moveText);
@@ -32,6 +31,20 @@ function App() {
         }
         setText(whyArr[randomIndex]);
     }
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("fade-up");
+                    }
+                });
+            },
+            { threshold: 0.01 }
+        );
+        const element = document.querySelector("#img-text-about-div");
+        observer.observe(element);
+    });
     return (
         <div>
             <section id="hero-section">
@@ -52,20 +65,19 @@ function App() {
                 </nav>
                 <div id="text-div">
                     <div id="inner-text-div">
-                    <div id="first-text">
-                        Wearing{" "}
-                        <span style={{ color: "black" }}>piercings </span>makes
-                        me
-                    </div>
-                    <div id="moving-text">
-                        <p
-                            id="moving-p"
-                            onAnimationIteration={() => triggerMove()}
-                        >
-                            {moveText}
-                        </p>
-                    </div>
-                
+                        <div id="first-text">
+                            Wearing{" "}
+                            <span style={{ color: "black" }}>piercings </span>
+                            makes me
+                        </div>
+                        <div id="moving-text">
+                            <p
+                                id="moving-p"
+                                onAnimationIteration={() => triggerMove()}
+                            >
+                                {moveText}
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div id="img-div">
@@ -80,8 +92,35 @@ function App() {
                 </div>
             </section>
             <section id="about-section">
-                <div id="about-header">
-                    About Us
+                <div id="about-div">
+                    <div id="about-header">
+                        <h2>About Us</h2>
+                    </div>
+                    <div id="img-text-about-div">
+                        <div id="about-img-div">
+                            <img
+                                id="about-img"
+                                src="https://images.unsplash.com/photo-1620891203092-971780cc3703?q=80&w=1059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            />
+                        </div>
+                        <div id="about-text">
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard dummy text ever since
+                                the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen
+                                book. It has survived not only five centuries,
+                                but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was
+                                popularised in the 1960s with the release of
+                                Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing
+                                software like Aldus PageMaker including versions
+                                of Lorem Ipsum.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section id="shop-section"></section>
