b612c16c5b3a38822a3e5d34eae10bed7a67afe3
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-04-01 01:02:50 +1300

    font, shop

--- src/App.tsx
+++ src/App.tsx
@@ -50,91 +50,148 @@ function App() {
     });
     return (
         <div>
-            <section id="hero-section">
-                <nav className="header">
-                    <ul>
-                        <li id="logo-div">
-                            <p id="logo-temp">Proud to Pierce</p>
-                        </li>
-                        {/* <li>About</li> */}
-                        {/* <li>Products</li> */}
-                        <li>
-                            <img className="nav-icons" src={cart} />
-                        </li>
-                        <li>
-                            <img className="nav-icons" src={search} />
-                        </li>
-                    </ul>
-                </nav>
-                <div id="text-div">
-                    <div id="inner-text-div">
-                        <div id="first-text">
-                            Wearing{" "}
-                            <span style={{ color: "black" }}>piercings </span>
-                            makes me
-                        </div>
-                        <div id="moving-text">
-                            <p
-                                id="moving-p"
-                                onAnimationIteration={() => triggerMove()}
-                            >
-                                {moveText}
-                            </p>
+            <div id="hero-wrapper">
+                <section id="hero-section">
+                    <nav className="header">
+                        <ul>
+                            <li id="logo-div">
+                                <p id="logo-temp">Proud to Pierce</p>
+                            </li>
+                            {/* <li>About</li> */}
+                            {/* <li>Products</li> */}
+                            <li>
+                                <img className="nav-icons" src={cart} />
+                            </li>
+                            <li>
+                                <img className="nav-icons" src={search} />
+                            </li>
+                        </ul>
+                    </nav>
+                    <div id="text-div">
+                        <div id="inner-text-div">
+                            <div id="first-text">
+                                Wearing{" "}
+                                <span style={{ color: "black" }}>
+                                    piercings{" "}
+                                </span>
+                                makes me
+                            </div>
+                            <div id="moving-text">
+                                <p
+                                    id="moving-p"
+                                    onAnimationIteration={() => triggerMove()}
+                                >
+                                    {moveText}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div id="img-div">
-                    <img
-                        id="hero-img"
-                        src="src/assets/placeholder-removebg.png"
-                    />
-                </div>
-                <div id="scroll-indicator">
-                    <p>Scroll</p>
-                    <div id="scroll-line"></div>
-                </div>
-            </section>
+                    <div id="img-div">
+                        <img
+                            id="hero-img"
+                            src="src/assets/placeholder-removebg.png"
+                        />
+                    </div>
+                    <div id="scroll-indicator">
+                        <p>Scroll</p>
+                        <div id="scroll-line"></div>
+                    </div>
+                </section>
+            </div>
+
             <section id="about-section">
                 <div id="who-we">WHO WE ARE</div>
                 <div id="about-div">
-                    <div id="about-header">
-                        <h2>Mcdonalds sushi tuna sashimi yadda</h2>
-                        <h2>Sushi tuna sashimi yadda</h2>
-                    </div>
                     <div id="img-text-about-div">
                         <div id="about-text">
+                            <div id="about-header">
+                                <h2>
+                                    Body piercings that give voice to how you
+                                    feel
+                                </h2>
+                            </div>
                             <p>
-                                Lorem Ipsum is simply dummy text of the printing
-                                and typesetting industry. Lorem Ipsum has been
-                                the industry's standard dummy text ever since
-                                the 1500s, when an unknown printer took a galley
-                                of type and scrambled it to make a type specimen
-                                book. It has survived not only five centuries,
-                                but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was
-                                popularised in the 1960s with the release of
-                                Letraset sheets containing Lorem Ipsum passages,
-                                and more recently with desktop publishing
-                                software like Aldus PageMaker including versions
-                                of Lorem Ipsum.
+                                Offering a wide range of affordable products
+                                <br>
+                                </br>
+                                Proud to Pierce is here for you!
                             </p>
                         </div>
                         <div id="about-img-div">
                             <img
                                 id="about-img"
-                                src="https://images.unsplash.com/photo-1620891203092-971780cc3703?q=80&w=1059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                src="https://images.unsplash.com/photo-1605740179653-9cc6fd743e04?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBpZXJjaW5nfGVufDB8fDB8fHww"
                             />
                             <img
                                 id="about-img"
-                                src="https://images.unsplash.com/photo-1620891203092-971780cc3703?q=80&w=1059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                src="https://images.unsplash.com/photo-1603323226047-df6de54ddc13?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGllcmNpbmd8ZW58MHx8MHx8fDI%3D"
                             />
                         </div>
                     </div>
                 </div>
             </section>
             <section id="shop-section">
+                <div id="who-we">SHOP NOW</div>
                 <div id="shop-div">
-                    <div id="shop-inner"></div>
+                    <div id="shop-inner">
+                        <div id="cards-div">
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgba(0, 255, 30, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgba(200, 0, 255, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgb(0, 115, 197, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgb(0, 115, 197, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgba(0, 255, 30, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                            <div
+                                className="shop-card"
+                                style={{ background: "rgba(200, 0, 255, 0.5)" }}
+                            >
+                                <div className="card-pic"></div>
+                                <div className="card-text-div">
+                                    Sort By Gauge
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
             <section id="insta-section">
