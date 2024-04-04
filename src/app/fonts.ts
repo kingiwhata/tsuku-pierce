3c9df403c37406850b22365c651ffba6e53f37e8
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-04-05 01:17:23 +1300

    routes

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,8 +1,15 @@
-import { Rock_Salt } from "next/font/google";
+import { Rock_Salt, Bebas_Neue } from "next/font/google";
 
 
 export const rockSalt = Rock_Salt({
     subsets: ["latin"],
     display: "swap",
     weight: "400",
+});
+
+export const bebasNeue = Bebas_Neue({
+    subsets: ["latin"],
+    weight: "400",
+    display: "auto",
+    variable: "--font-bebas"
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
-import {  Bebas_Neue } from "next/font/google";
-
-
-const bebasNeue = Bebas_Neue({
-    subsets: ["latin"],
-    weight: "400",
-    display: "auto",
-    variable: "--font-bebas"
-});
+import { bebasNeue } from "./fonts";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const metadata: Metadata = {
@@ -28,7 +20,6 @@ export default function RootLayout({
                     href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap"
                     rel="stylesheet"
                 />
-                <title>Vite + React + TS</title>
             </head>
             <body>
                 <div id="root">{children}</div>
